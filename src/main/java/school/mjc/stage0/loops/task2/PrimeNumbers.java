7317158a5c0b376ded12d935c65e312d3ad76d81
Anastasiya P <anastasiapatsei@gmail.com>
2023-02-12 19:15:39 +0200

    partially completed

--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,11 +2,18 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
-      int count = 1;
+        for( int startNumber = 2; startNumber <= printToInclusive; startNumber++){
+            boolean isPrime = true;
+            for(int factor = 2; factor < startNumber; factor++){
+                if (startNumber % factor == 0){
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime){
+                System.out.println(startNumber);
+            }
+        }
 
-      while(count <= printToInclusive){
-          System.out.println(count);
-          count++;
-      }
     }}
 
